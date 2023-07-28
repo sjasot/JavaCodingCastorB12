323a50d90eab955ededcdd50ab16ae31bb3ebb00
sjasot <sjasot@icloud.com>
2023-07-28 11:48:18 -0400

    Add week_1 to Serhiy

---
+++ src/Serhiy/week_1/DivisionWithoutOperator.java
@@ -0,0 +1,7 @@
+package Serhiy.week_1;
+public class DivisionWithoutOperator {
+    /*
+Numbers - Divide without / operator:
+Write a method that can divide two numbers without using division operator
+     */
+}
