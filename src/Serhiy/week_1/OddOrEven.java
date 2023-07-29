e782176536c7c5f5db402246ea7ef98116a30457
sjasot <sjasot@icloud.com>
2023-07-29 17:55:30 -0400

    included oddOrEven method in OddOrEven wk 1

--- src/Serhiy/week_1/OddOrEven.java
+++ src/Serhiy/week_1/OddOrEven.java
@@ -8,4 +8,20 @@ Output ex:
 identify(5); ->"Odd"
 identify(6); ->"Even"
      */
+
+    public static void main(String[] args) {
+
+        OddOrEven.EvenOrOdd(5);
+        OddOrEven.EvenOrOdd(6);
+    }
+    public static void EvenOrOdd (int num){
+        if (num % 2 == 0){
+            System.out.println(num +": Even");
+        } else if (num % 2 != 0) {
+            System.out.println(num +": Odd");
+        }
+
+    }
+
+
 }
