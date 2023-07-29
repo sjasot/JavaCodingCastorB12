7fcf6e1f9c679fd47141623594a7f47ec1b339ab
sjasot <sjasot@icloud.com>
2023-07-29 18:50:09 -0400

    added solution to Finra wk 1

--- src/Serhiy/week_1/Finra.java
+++ src/Serhiy/week_1/Finra.java
@@ -10,16 +10,17 @@ public class Finra {
     //approach 1
     public static void main(String[] args) {
 
+
         for (int i = 1; i <= 30; i++) {
 
-            if (i%15==0 ) {
+            if (i % 15 == 0) {
                 System.out.print("FINRA, ");
-            }else if (i%5 == 0) {
+            } else if (i % 5 == 0) {
                 System.out.print("RA, ");
-            } else if (i%3 == 0) {
+            } else if (i % 3 == 0) {
                 System.out.print("FIN, ");
-            }else {
-                System.out.print(i+", ");
+            } else {
+                System.out.print(i + ", ");
             }
         }
 
@@ -43,8 +44,9 @@ public class Finra {
         System.out.println(result);
 
          */
+
+
+
     }
-
-
 
 }
